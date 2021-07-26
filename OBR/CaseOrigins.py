5f7ba80293b8b637a6ae957aa3df2457207cf17f
Gregor Olenik <gregor.olenik@web.de>
2021-07-26 08:06:37 +0200

    add case origins

---
+++ OBR/CaseOrigins.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+from subprocess import check_output
+
+
+class CaseOrigin:
+    def __init__(self, args):
+        self.args = args
+
+    def copy_to(self, dst):
+        cmd = ["cp", "-r", self.path, dst]
+        print(cmd)
+        check_output(cmd)
+
+
+class OpenFOAMTutorialCase(CaseOrigin):
+    def __init__(self, args_dict):
+        super().__init__(args_dict)
+        self.tutorial_domain = self.args["origin"]
+        self.solver = self.args["solver"]
+        self.case = self.args["case"]
+
+    @property
+    def path(self):
+        foam_tutorials = Path(os.environ["FOAM_TUTORIALS"])
+        return foam_tutorials / self.tutorial_domain / self.solver / self.case
+
+
+class OpenFOAMExternalCase(CaseOrigin):
+    def __init__(self, args_dict):
+        super().__init__(args_dict)
+        self.path = path
+        self.solver = solver
+        self.case = case
+
+
+class TestCase(CaseOrigin):
+    def __init__(self, args_dict):
+        super().__init__(args_dict)
+        self.path_ = path
+        self.solver = solver
+
+    @property
+    def path(self):
+        return Path(self.path_)
