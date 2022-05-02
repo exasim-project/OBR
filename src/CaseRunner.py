86cc5f4412cc4c4febc9fde99db1895fbcc1dbbc
Gregor Olenik <go@hpsim.de>
2022-05-02 11:44:06 +0200

    Add ResultsCollector

--- src/CaseRunner.py
+++ src/CaseRunner.py
@@ -79,6 +79,46 @@ class SlurmCaseRunner:
         check_output(sbatch_cmd, cwd=run_path)
 
 
+class ResultsCollector:
+    def __init__(self, results_aggregator, arguments):
+        self.results = results_aggregator
+        self.arguments = arguments
+        self.log_name = arguments["log_name"]
+
+    def hash_and_store_log(self, ret, path, log_fold):
+        log_hash = hashlib.md5(ret).hexdigest()
+        log_path = path / self.log_name
+        log_path = log_path.with_suffix(".log")
+        log_str = ret.decode("utf-8")
+        log_file = log_fold / self.log_name
+        with open(log_file, "a") as log_handle:
+            print("writing to log", log_file, type(log_str))
+            log_str_ = "hash: {}\n{}{}\n".format(log_hash, log_str, "=" * 80)
+            log_handle.write(log_str_)
+        return log_hash
+
+    def run(self, path, execution_parameter, case_parameter):
+        import time
+        from pathlib import Path
+
+        run_path = Path(path)
+
+        case = OpenFOAMCase(run_path)
+        sub_domains = sf.get_number_of_subDomains(case.path)
+
+        with open(run_path / "log") as fh:
+            ret = fh.read()
+        log_hash = self.hash_and_store_log(ret, case.path, self.results.log_fold)
+
+        self.results.set_case(case, execution_parameter, case_parameter)
+
+        self.results.add(
+            timestamp=str(datetime.datetime.utcnow()),
+            log_id=log_hash,
+            run_time=0,
+        )
+
+
 class LocalCaseRunner:
     def __init__(self, results_aggregator, arguments):
         self.results = results_aggregator
