0832a48cba40dd234a92d047dca88aa488328990
Gregor Olenik <gregor.olenik@web.de>
2023-02-01 08:46:06 +0100

    improve output

--- src/core.py
+++ src/core.py
@@ -27,13 +27,18 @@ def logged_execute(cmd, path, doc):
     """
     res = doc.get("obr", {})
     cmd_str = " ".join(cmd)
-    # print("execute shell command: ", cmd)
     try:
         ret = check_output(cmd, cwd=path, stderr=subprocess.STDOUT).decode("utf-8")
         cmd_str = cmd_str.replace(".", "_dot_")
         res[cmd_str] = {"log": ret, "state": "success"}
     except subprocess.SubprocessError as e:
-        print("SubprocessError:", __file__, __name__, e, e.output, e.stderr)
+        print(
+            "SubprocessError:",
+            __file__,
+            __name__,
+            e,
+            " check: 'obr find --state failure' for more info",
+        )
         log = e.output.decode("utf-8")
         res[cmd_str] = {"log": log, "state": "failure"}
     except FileNotFoundError as e:
