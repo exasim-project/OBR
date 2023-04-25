2595084d670e5d6ef592297aa4a50c03085cd688
Gregor Olenik <gregor.olenik@web.de>
2023-04-25 10:49:51 +0200

    Fixup missing import

--- src/obr/cli.py
+++ src/obr/cli.py
@@ -23,6 +23,7 @@ from .signac_wrapper.labels import *
 from .signac_wrapper.operations import *
 from .create_tree import create_tree
 from .core.parse_yaml import read_yaml
+from .core.queries import input_to_queries
 
 
 @click.group()
