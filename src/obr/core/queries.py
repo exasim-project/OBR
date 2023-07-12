beb2b0e050cb997bbdc22e08e456ce53b1512d9f
Gregor Olenik <gregor.olenik@web.de>
2023-07-12 14:17:11 +0200

    Fixup

--- src/obr/core/queries.py
+++ src/obr/core/queries.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any
+from typing import Any, Union, Callable
 from copy import deepcopy
 import re
 from obr.signac_wrapper.operations import OpenFOAMProject
