2be9b34339c6d3f5238935244bf3567057f35ef2
Lukas Petermann <lukas.petermann13@gmail.com>
2023-07-27 18:41:51 +0200

    remove <<HEAD, black formatting

--- src/obr/core/core.py
+++ src/obr/core/core.py
@@ -175,6 +175,7 @@ def modifies_file(fns):
     else:
         unlink(fns)
 
+
 def check_log_for_success(log: Path) -> bool:
     res = check_output(["tail", "-n", "2", log], text=True)
     return ("Finalising" in res) or ("End" in res)
