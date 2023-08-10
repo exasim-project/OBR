e462cb690a0d83830fd866621110849adc6edd53
Gregor Olenik <go@hpsim.de>
2023-08-10 13:23:22 +0200

    add merge_job_doc implementation

--- src/obr/core/core.py
+++ src/obr/core/core.py
@@ -231,16 +231,30 @@ def writes_files(fns):
 
 def merge_job_documents(job):
     """Merge multiple job_document_hash.json files into job_document.json"""
-    _, _, files = next(os.walk(job.path))
+    root, _, files = next(os.walk(job.path))
+    print(files)
 
     def is_job_sub_document(fn):
-        if fn == "job_document.json":
+        if fn == "signac_job_document.json":
             return False
-        return fn.startswith("job_document")
+        return fn.startswith("signac_job_document")
 
     files = [f for f in files if is_job_sub_document(f)]
-    print(files)
-    job.doc = {}
+    merged_data = []
+    merged_history = []
+    cache = None
+    for f in files:
+        with open(Path(root)/f, "r") as fh:
+            job_doc = json.load(fh)
+            for record in job_doc["data"]:
+                merged_data.append(record)
+            for record in job_doc["history"]:
+                merged_history.append(record)
+            # TODO handle inconsistent cache
+            if not cache:
+                cache = job_doc["cache"]
+    merged_job_doc = {"data": merged_data, "history": merged_history, "cache": cache}
+    job.doc = merged_job_doc
 
 
 def map_view_folder_to_job_id(view_folder: str) -> dict[str, str]:
