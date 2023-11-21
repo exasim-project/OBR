8cca06194cd517898b76f0644407acf217c75c1e
Lukas Petermann <lukas.petermann13@gmail.com>
2023-11-21 15:37:47 +0100

    version

--- setup.py
+++ setup.py
@@ -21,7 +21,9 @@ def read(*names, **kwargs):
 
 
 setup(
-    version='0.3.0',
+    name="obr",
+    license="BSD-2-Clause",
+    description="A tool to create and run OpenFOAM parameter studies",
     long_description="%s\n%s"
     % (
         re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
@@ -29,17 +31,18 @@ setup(
         ),
         re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
+    author="Gregor Olenik",
+    author_email="go@hpsim.de",
+    url="https://github.com/hpsim/obr",
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
     classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: Unix",
         "Operating System :: POSIX",
@@ -54,8 +57,26 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
+        # uncomment if you test on these interpreters:
+        # 'Programming Language :: Python :: Implementation :: IronPython',
+        # 'Programming Language :: Python :: Implementation :: Jython',
+        # 'Programming Language :: Python :: Implementation :: Stackless',
         "Topic :: Utilities",
     ],
+    project_urls={
+        "Documentation": "https://obr.readthedocs.io/",
+        "Changelog": "https://obr.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/hpsim/obr/issues",
+    },
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    python_requires=">=3.6",
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+    },
     entry_points={
         "console_scripts": [
             "obr = obr.cli:main",
