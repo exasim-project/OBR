68eee8a9a7cfd37d71ee10ea114b67f1ed3799aa
Gregor Olenik <go@hpsim.de>
2023-11-21 14:46:40 +0100

    Update src/obr/OpenFOAM/case.py
    Co-authored-by: Lukas Petermann <60653355+lupeterm@users.noreply.github.com>

--- src/obr/OpenFOAM/case.py
+++ src/obr/OpenFOAM/case.py
@@ -245,6 +245,7 @@ class OpenFOAMCase(BlockMesh):
             )
 
         method = args["method"]
+        numberSubDomains = int(args.get("numberOfSubdomains", 0))
         if method == "simple":
             if not args.get("numberOfSubdomains"):
                 coeffs = [int(i) for i in args["coeffs"]]
