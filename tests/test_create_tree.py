08d097c830079709320f59e97d8572e68298cd95
Gregor Olenik <gregor.olenik@web.de>
2024-01-14 21:19:13 +0100

    move to mirror of develop.openfoam.com to avoid failing tests

--- tests/test_create_tree.py
+++ tests/test_create_tree.py
@@ -14,7 +14,7 @@ def emit_test_config():
         "case": {
             "type": "GitRepo",
             "solver": "pisoFoam",
-            "url": "https://github.com/exasim-project/hpc.git"
+            "url": "https://github.com/exasim-project/hpc.git",
             "folder": "Lid_driven_cavity-3d/S",
             "commit": "f9594d16aa6993bb3690ec47b2ca624b37ea40cd",
             "cache_folder": "None/S",
