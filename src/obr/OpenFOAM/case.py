b555a772ca336c8b9fff2b4b85de6a5c1828ce87
Gregor Olenik <gregor.olenik@web.de>
2024-09-04 09:39:59 +0200

    fix force int for subdomain

--- src/obr/OpenFOAM/case.py
+++ src/obr/OpenFOAM/case.py
@@ -333,7 +333,7 @@ class OpenFOAMCase(BlockMesh):
             )
 
         method = args["method"]
-        numberSubDomains = int(args.get("numberOfSubdomains", 0))
+        numberSubDomains = int(float(args.get("numberOfSubdomains", 0)))
         if method == "simple":
             distribute = args.get("simple", "distribute")
             if distribute == "distributed":
