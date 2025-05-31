a96d453f43c58555f43189969bd0d5e635589744
Gregor Olenik <gregor.olenik@web.de>
2025-05-31 14:28:50 +0200

    wip add parse queries

--- src/obr/core/parse_yaml.py
+++ src/obr/core/parse_yaml.py
@@ -60,14 +60,14 @@ def parse_special_variables(in_str: str, args: dict, domain: str, verbose: bool)
         )
     return in_str
 
-def parse_queries(in_str: str, args: dict) -> str:
-    """Replaces ${{ get.value }} expressions with environmental variable values"""
+def parse_queries(in_str: str, args: dict, domain: str) -> str:
+    """Replaces ${{ domain.value }} expressions with environmental variable values"""
     ocurrances = re.findall(r"\${{get" + r"\.(\w+)}}", in_str)
     for inst in ocurrances:
         if not args.get(inst, ""):
-            logger.warning(f"warning get.{inst} query failed")
+            logger.warning(f"warning {inst} not defined")
         in_str = in_str.replace(
-        "${{get." + inst + "}}", str(args.get(inst, f"'{inst}'"))
+            "${{" + domain + "." + inst + "}}", args.get(inst, f"'{inst}'")
         )
     return in_str
 
