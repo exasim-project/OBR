b8e69d85a52c95c047803a61712e223b2b452b38
Gregor Olenik <go@hpsim.de>
2025-08-06 14:21:26 +0200

    WIP, fixup create tree

--- src/obr/create_tree.py
+++ src/obr/create_tree.py
@@ -174,10 +174,10 @@ def expand_generator_block(operation, base_dict={}):
                 # next k, v are the key values from the template record
                 # not to confused with the key value pair from the generator block
                 for k, v in template.items():
-                    # if isinstance(v, str):
-                    #     # handle get. queries
-                    #     v = parse_queries(v, base_dict)
-                    #     gen_dict[k] = v.replace(key, str(val))
+                    if isinstance(v, str):
+                        # handle get. queries
+                        # v = parse_queries(v, base_dict)
+                        gen_dict[k] = v.replace(key, str(val))
                     # additionally the original key and current
                     # val are added so that we can use it in schemas
                     gen_dict[key] = val
